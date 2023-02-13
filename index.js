77c59a054a01e72629dcecf8d095fd72531052e0
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2023-02-13 10:37:15 -0800

    Remove typescript dependency
    Turns out I didn't need it to repro the oom.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { readdirSync } from 'fs';
 import { join } from 'path';
 import { ESLint } from "eslint";
-// import { clearProgramCache } from "@typescript-eslint/typescript-estree";
+import { clearProgramCache } from "@typescript-eslint/typescript-estree";
 const cwd = process.cwd();
-// import ts from 'typescript'
 for (const dirPath of readdirSync('../../DefinitelyTyped/types')) {
     const dir = join('../../DefinitelyTyped/types', dirPath);
     process.chdir(dir);
@@ -13,8 +12,7 @@ for (const dirPath of readdirSync('../../DefinitelyTyped/types')) {
     const esfiles = ["index.d.ts"];
     const formatter = await eslint.loadFormatter("stylish");
     const eresults = await eslint.lintFiles(esfiles);
-    console.log(formatter.format(eresults))
-    // console.log(ts.sys.getMemoryUsage?.())
-    // clearProgramCache();
+    console.log(formatter.format(eresults), process.memoryUsage().heapUsed)
+    clearProgramCache();
     process.chdir(cwd);
 }
